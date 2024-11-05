7a77fbb906fe61e6d4a57c7f49ed6d7d84ca3441
Fariskt <fariskt00@gmail.com>
2024-11-05 11:36:16 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allow external access
+    port: process.env.PORT || 3000, // Use PORT from environment variable, default to 3000
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for production builds
+  },
+});
